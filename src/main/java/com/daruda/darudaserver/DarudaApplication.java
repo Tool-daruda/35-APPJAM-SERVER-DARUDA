1d8f514d3eee941325138503cb8b96e2eef9d74f
jjuny-won <jjuny040627@gamil.com>
2025-01-01 03:18:00 +0900

    #0 [chore] 초기 세팅

---
+++ src/main/java/com/daruda/darudaserver/DarudaApplication.java
@@ -0,0 +1,13 @@
+package com.daruda.darudaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarudaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daruda/darudaserver/DarudaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daruda.darudaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarudaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
