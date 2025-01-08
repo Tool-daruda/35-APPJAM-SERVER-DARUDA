c80710eeae6a2cb4279c1ee2601d364fd139478a
woals2840 <woals2840@gmail.com>
2025-01-08 15:26:56 +0900

    #5 [chore] CustomAccessDeniedHandler 구현

---
+++ src/main/java/com/daruda/darudaserver/global/auth/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,23 @@
+package com.daruda.darudaserver.global.auth.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        String path = request.getRequestURI();
+        String method = request.getMethod();
+        setResponse(response);
+    }
+
+    private void setResponse(HttpServletResponse response) {
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+    }
+}
