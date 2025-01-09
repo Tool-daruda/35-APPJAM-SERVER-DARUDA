036d543b3e477f0ef6548e09c2bca7c38ddc921e
jjuny-won <jjuny040627@gamil.com>
2025-01-09 15:34:54 +0900

    #18 [feat] ToolDTO 추가

---
+++ src/main/java/com/daruda/darudaserver/domain/tool/controller/ToolController.java
@@ -0,0 +1,8 @@
+package com.daruda.darudaserver.domain.tool.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ToolController {
+
+}
---
+++ src/main/java/com/daruda/darudaserver/domain/tool/service/ToolService.java
@@ -0,0 +1,8 @@
+package com.daruda.darudaserver.domain.tool.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ToolService {
+
+}
