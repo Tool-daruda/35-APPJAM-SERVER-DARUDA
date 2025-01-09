7b7e9abf08ec2281b890d8408ad6f0c3a82c0d22
jjuny-won <jjuny040627@gamil.com>
2025-01-09 12:23:05 +0900

    #15 [feat] 게시글 삭제 구현-1

--- src/main/java/com/daruda/darudaserver/domain/community/dto/request/BoardCreateAndUpdateReq.java
+++ src/main/java/com/daruda/darudaserver/domain/community/dto/request/BoardCreateAndUpdateReq.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Builder(access = AccessLevel.PROTECTED)
 public record BoardCreateAndUpdateReq(
         @NotNull(message="제목 입력 필수 입니다.")
-        @Size(min=1, max=20, message = "제목은 최소 한 글자, 최대 20글자이어야 합니다") //최소 한 글자 ,띄어쓰기 포함 최대 20자 가능
+        @Size(min=1, max=20, message = "제목은 최소 한 글자, 최대 20글자이어야 합니다")//최소 한 글자 ,띄어쓰기 포함 최대 20자 가능
         String title,
         @Size(min=1, max=10000) //최소 한 글자 ,띄어쓰기 포함 최대 10000자 가능
         @NotNull(message="내용  입력은 필수 입니다.")
