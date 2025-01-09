c74ee2f506ce969e2bf84dd1a711f7319b1c994e
woals2840 <woals2840@gmail.com>
2025-01-10 01:54:49 +0900

    #20 [feat] 로그인 요청 Controller 구현

---
+++ src/main/java/com/daruda/darudaserver/domain/auth/controller/KakaoController.java
@@ -0,0 +1,27 @@
+package com.daruda.darudaserver.domain.auth.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RequestMapping("/api/v1/users")
+@RestController
+@RequiredArgsConstructor
+public class KakaoController {
+    @Value("${KAKAO_CLIENT_ID")
+    private String client_id;
+
+    @Value("${kakao.redirect_uri")
+    private String redirect_uri;
+
+    @GetMapping("/kako/login-url")
+    public RedirectView requestLogin(){
+        RedirectView redirectView = new RedirectView();
+        String location = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id="+client_id+"&redirect_uri="+redirect_uri;
+        redirectView.setUrl(location);
+        return redirectView;
+    }
+}
