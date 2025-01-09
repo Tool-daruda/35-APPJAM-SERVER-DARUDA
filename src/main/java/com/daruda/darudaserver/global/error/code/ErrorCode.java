bd1f96d685f3ec84f5632fa139e79e89c4d5412a
woals2840 <woals2840@gmail.com>
2025-01-10 02:37:15 +0900

    #5 [chore] 에러코드 병합 및 커스텀

--- src/main/java/com/daruda/darudaserver/global/error/code/ErrorCode.java
+++ src/main/java/com/daruda/darudaserver/global/error/code/ErrorCode.java
@@ -15,9 +15,17 @@ public enum ErrorCode {
     INVALID_REQUEST_BODY(HttpStatus.BAD_REQUEST, "E400006","요청 본문이 올바르지 않습니다"),
     DATA_INTEGRITY_VIOLATION(HttpStatus.BAD_REQUEST, "E400007","데이터 무결성 제약 조건을 위반했습니다"),
     BUSINESS_LOGIC_ERROR(HttpStatus.BAD_REQUEST, "E400008","비즈니스 로직 처리 중 오류가 발생했습니다"),
-    INVALID_IMAGE_TYPE(HttpStatus.BAD_REQUEST, "E400009", "지원하지 않는 이미지 확장자 입니다."),
-    INVALID_IMAGE_SIZE(HttpStatus.BAD_REQUEST, "E400010", "지원하지 않는 이미지 크기 입니다."),
-    WRONG_IMAGE_URL(HttpStatus.BAD_REQUEST, "E400011", "잘못된 이미지 URL 입니다."),
+    REFRESH_TOKEN_USER_ID_MISMATCH_ERROR(HttpStatus.BAD_REQUEST,"E400009", "리프레쉬 토큰의 사용자 정보가 일치하지 않습니다"),
+    INVALID_REFRESH_TOKEN_ERROR(HttpStatus.BAD_REQUEST,"E400010", "잘못된 리프레쉬 토큰입니다"),
+    REFRESH_TOKEN_SIGNATURE_ERROR(HttpStatus.BAD_REQUEST,"E400011", "리프레쉬 토큰의 서명이 잘못되었습니다"),
+    UNSUPPORTED_REFRESH_TOKEN_ERROR(HttpStatus.BAD_REQUEST,"E400012", "지원하지 않는 리프레쉬 토큰입니다"),
+    REFREH_TOKEN_EMPTY_ERROR(HttpStatus.BAD_REQUEST,"E400013", "리프레쉬 토큰이 비어있습니다"),
+
+    /* 401 */
+    AUTHENTICATION_CODE_EXPIRED(HttpStatus.UNAUTHORIZED,"E401001", "인가코드가 만료되었습니다"),
+    REFRESH_TOKEN_EXPIRED_ERROR(HttpStatus.UNAUTHORIZED,"E401002","리프레쉬 토큰이 만료되었습니다"),
+
+
     /* 404 NOT FOUND */
 
     DATA_NOT_FOUND(HttpStatus.NOT_FOUND, "E404001","데이터가 존재하지 않습니다"),
