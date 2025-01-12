afb7fd20273c063fa6186fde727b05afe38dd4d8
woals2840 <woals2840@gmail.com>
2025-01-13 00:19:26 +0900

    #30 [feat] Config 추가

--- src/main/java/com/daruda/darudaserver/global/config/WebConfig.java
+++ src/main/java/com/daruda/darudaserver/global/config/WebConfig.java
@@ -1,11 +1,18 @@
 package com.daruda.darudaserver.global.config;
 
+import com.daruda.darudaserver.global.auth.UserIdArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+    private final UserIdArgumentResolver userIdArgumentResolver;
 
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry){
@@ -16,4 +23,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000);
     }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers){
+        resolvers.add(userIdArgumentResolver);
+    }
+
 }
