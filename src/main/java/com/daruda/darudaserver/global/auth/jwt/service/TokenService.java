f02b58a2b2e81ea1351eb734a6c133512de5ab13
woals2840 <woals2840@gmail.com>
2025-01-12 17:44:58 +0900

    #20 [feat] Token 수정

--- src/main/java/com/daruda/darudaserver/global/auth/jwt/service/TokenService.java
+++ src/main/java/com/daruda/darudaserver/global/auth/jwt/service/TokenService.java
@@ -7,19 +7,25 @@ import com.daruda.darudaserver.global.error.exception.InvalidValueException;
 import com.daruda.darudaserver.global.error.exception.NotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class TokenService {
     private final TokenRepository tokenRepository;
 
+
     @Transactional
     public void saveRefreshtoken(final Long userId, final String refreshToken){
-        tokenRepository.save(Token.of(userId, refreshToken));
+        tokenRepository.save(Token.builder()
+                .userId(userId)
+                .refreshToken(refreshToken)
+                .build());
     }
 
+    @Transactional
     public Long findIdByRefreshToken(final String refreshToken){
         Token token = tokenRepository.findByRefreshToken(refreshToken)
                 .orElseThrow(() -> new InvalidValueException(ErrorCode.REFRESH_TOKEN_EMPTY_ERROR));
@@ -28,9 +34,10 @@ public class TokenService {
 
     @Transactional
     public void deleteRefreshToken(final Long userId){
-        Token token = tokenRepository.findById(userId)
+        Token token = tokenRepository.findByUserId(userId)
                 .orElseThrow(()-> new NotFoundException(ErrorCode.REFRESH_TOKEN_NOT_FOUND));
 
         tokenRepository.delete(token);
     }
+
 }
