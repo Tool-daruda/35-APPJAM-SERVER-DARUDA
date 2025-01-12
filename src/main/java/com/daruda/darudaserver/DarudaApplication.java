fdf5247cb14a2c6d363e577356b05e7385d9f4c9
woals2840 <woals2840@gmail.com>
2025-01-12 17:42:55 +0900

    #20 [feat] FeignClient 사용

--- src/main/java/com/daruda/darudaserver/DarudaApplication.java
+++ src/main/java/com/daruda/darudaserver/DarudaApplication.java
@@ -2,10 +2,10 @@ package com.daruda.darudaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
-@EnableJpaAuditing //시간 자동 생성을 위해서는 Auditing 기능 활성화
 public class DarudaApplication {
 
 	public static void main(String[] args) {
