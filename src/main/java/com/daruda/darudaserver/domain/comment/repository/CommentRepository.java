a052e3b5c0d668b64e4644592774faf49bbe4035
woals2840 <woals2840@gmail.com>
2025-01-15 20:58:51 +0900

    #51 [fix] s3 이미지 저장

--- src/main/java/com/daruda/darudaserver/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/daruda/darudaserver/domain/comment/repository/CommentRepository.java
@@ -2,6 +2,8 @@ package com.daruda.darudaserver.domain.comment.repository;
 
 import com.daruda.darudaserver.domain.comment.entity.CommentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends JpaRepository<CommentEntity,Long> {
 }
