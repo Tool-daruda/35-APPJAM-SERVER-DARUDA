293f21b943db7c260e0d292e53b616fdeef3bd67
woals2840 <woals2840@gmail.com>
2025-01-16 04:03:14 +0900

    #61 [fix] 성공 응답 코드 추가

--- src/main/java/com/daruda/darudaserver/global/error/code/SuccessCode.java
+++ src/main/java/com/daruda/darudaserver/global/error/code/SuccessCode.java
@@ -7,12 +7,15 @@ import org.springframework.http.HttpStatus;
 public enum SuccessCode {
     /* 201 CREATED */
     SUCCESS_CREATE(HttpStatus.CREATED, "생성이 완료되었습니다"),
+    SUCCESS_REISSUE(HttpStatus.CREATED,"토큰이 재발급되었습니다"),
 
     /* 200 OK */
     SUCCESS_UPDATE(HttpStatus.OK, "업데이트가 완료되었습니다"),
     SUCCESS_DELETE(HttpStatus.OK, "삭제가 완료되었습니다"),
     SUCCESS_FETCH(HttpStatus.OK, "요청 데이터가 성공적으로 조회되었습니다"),
-    SUCCESS_SCRAP(HttpStatus.OK,  "스크랩이 변경이 완료되었습니다");
+    SUCCESS_SCRAP(HttpStatus.OK,  "스크랩이 변경이 완료되었습니다"),
+    SUCCESS_LOGIN(HttpStatus.OK, "로그인 성공했습니다"),
+    SUCCESS_LOGUT(HttpStatus.OK,"로그아웃에 성공했습니다");
 
     private final HttpStatus httpStatus;
     private final String message;
