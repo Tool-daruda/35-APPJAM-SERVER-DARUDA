9430c777dabf08dc9f18b9ffc447c6a1cc58a8c1
woals2840 <woals2840@gmail.com>
2025-01-16 04:59:41 +0900

    #61 [fix] 무한 스크롤로 DTO 변경

--- src/main/java/com/daruda/darudaserver/domain/community/repository/BoardRepository.java
+++ src/main/java/com/daruda/darudaserver/domain/community/repository/BoardRepository.java
@@ -1,13 +1,14 @@
 package com.daruda.darudaserver.domain.community.repository;
 
 import com.daruda.darudaserver.domain.community.entity.Board;
-import feign.Param;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import com.daruda.darudaserver.domain.tool.entity.Tool;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
