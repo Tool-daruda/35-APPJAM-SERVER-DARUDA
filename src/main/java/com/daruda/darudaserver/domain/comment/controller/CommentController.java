a80cb7dccf290f373359dbcf6712c854d3da1e69
woals2840 <woals2840@gmail.com>
2025-01-15 17:06:22 +0900

    #51 [feat] 댓글작성 Controller 구현

---
+++ src/main/java/com/daruda/darudaserver/domain/comment/controller/CommentController.java
@@ -0,0 +1,27 @@
+package com.daruda.darudaserver.domain.comment.controller;
+import com.daruda.darudaserver.domain.comment.dto.request.CreateCommentRequest;
+import com.daruda.darudaserver.domain.comment.dto.response.CreateCommentResponse;
+import com.daruda.darudaserver.domain.comment.service.CommentService;
+import com.daruda.darudaserver.global.auth.UserId;
+import com.daruda.darudaserver.global.common.response.ApiResponse;
+import com.daruda.darudaserver.global.error.code.SuccessCode;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/comments")
+public class CommentController {
+    private final CommentService commentService;
+
+    @PostMapping
+    public ResponseEntity<?> postComment(@UserId Long userId,
+                                         @RequestParam("board-id")Long boardId,
+                                         @Valid @RequestBody CreateCommentRequest createCommentRequest){
+        CreateCommentResponse createCommentResponse = commentService.postComment(userId, boardId, createCommentRequest);
+        return ResponseEntity.ok(ApiResponse.ofSuccessWithData(createCommentResponse, SuccessCode.SUCCESS_CREATE));
+    }
+
+}
