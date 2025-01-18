b9695f01c56768259d5f6d49aa6244f966bec923
jjuny-won <101424642+jjuny-won@users.noreply.github.com>
2025-01-19 04:35:50 +0900

    #99 [fix] 카테고리 반환 순서 수정 (#100)
    * #99 [fix] 카테고리 반환 순서 수정
    
    * #99 [fix] Gitignore 수정
    
    * #99 [fix] 로그 파일 제거
    
    ---------
    
    Co-authored-by: jjuny-won <jjuny040627@gamil.com>

--- src/main/java/com/daruda/darudaserver/domain/tool/entity/Category.java
+++ src/main/java/com/daruda/darudaserver/domain/tool/entity/Category.java
@@ -9,34 +9,22 @@ import java.util.Arrays;
 @Getter
 public enum Category {
     ALL("전체"),
-    LIFESTYLE("생활"),
-    DOCUMENT_EDITING("문서 작성/편집"),
     AI("AI"),
-    COLLABORATION("협업/커뮤니케이션"),
-    CODING("코딩/개발"),
-    DESIGN_MODELING("설계/모델링"),
-    DATA("데이터"),
+    DOCUMENT_EDITING("문서 작성/편집"),
     PRESENTATION("프레젠테이션"),
+    COLLABORATION("협업/커뮤니케이션"),
+    DATA("데이터"),
     GRAPHIC_DESIGN("그래픽/디자인"),
     VIDEO_MUSIC("영상/음악"),
+    CODING("코딩/개발"),
+    DESIGN_MODELING("설계/모델링"),
+    LIFESTYLE("생활"),
     CAREER_DEVELOPMENT("커리어/자기개발");
 
     private final String koreanName;
 
     Category(String koreanName) {
         this.koreanName = koreanName;
-    }
-
-    public static Category fromKoreanName(String name) {
-        // Null 또는 빈 문자열의 경우 기본값으로 Category.ALL 반환
-        if (name == null || name.isBlank()) {
-            return Category.ALL;
-        }
-        // 한국어 이름을 기반으로 Category 검색
-        return Arrays.stream(values())
-                .filter(category -> category.koreanName.equalsIgnoreCase(name.trim()))
-                .findFirst()
-                .orElseThrow(() -> new InvalidValueException(ErrorCode.INVALID_TOOL_CATEGORY));
     }
 
 
