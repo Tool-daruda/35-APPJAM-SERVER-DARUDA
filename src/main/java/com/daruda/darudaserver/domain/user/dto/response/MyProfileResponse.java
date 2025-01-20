d969847668992ce1468334695597405c7a3c080e
woals2840 <woals2840@gmail.com>
2025-01-21 02:06:29 +0900

    #109 [fix]내 정보 조회 API 추가

---
+++ src/main/java/com/daruda/darudaserver/domain/user/dto/response/MyProfileResponse.java
@@ -0,0 +1,10 @@
+package com.daruda.darudaserver.domain.user.dto.response;
+
+public record MyProfileResponse(
+        String nickname,
+        String positions
+) {
+    public static MyProfileResponse of(String nickname, String positions){
+        return new MyProfileResponse(nickname, positions);
+    }
+}
