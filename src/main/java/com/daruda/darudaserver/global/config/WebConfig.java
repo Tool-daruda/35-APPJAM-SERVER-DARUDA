683cf0164ea46c676a0f998db26bde88dd00edc6
woals2840 <woals2840@gmail.com>
2025-01-20 22:43:45 +0900

    #103 [fix] @EnableWebMvc

--- src/main/java/com/daruda/darudaserver/global/config/WebConfig.java
+++ src/main/java/com/daruda/darudaserver/global/config/WebConfig.java
@@ -5,12 +5,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     private final UserIdArgumentResolver userIdArgumentResolver;
 
