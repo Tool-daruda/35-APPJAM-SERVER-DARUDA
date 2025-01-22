bdf13b3821ea9eb394650a239ec87dc2e387b626
jjuny-won <jjuny040627@gamil.com>
2025-01-23 06:46:58 +0900

    #157 [fix] security 수정

--- src/main/java/com/daruda/darudaserver/global/config/SecurityConfig.java
+++ src/main/java/com/daruda/darudaserver/global/config/SecurityConfig.java
@@ -23,15 +23,14 @@ public class SecurityConfig {
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtTokenProvider jwtTokenProvider;
 
-    private static final String[] WHITE_LIST = {"/api/v1/boards/board/**",
+    private static final String[] WHITE_LIST = {
             "/api/v1/users/signin",
             "/api/v1/users/token",
             "/api/v1/users/signup",
             "/api/v1/users/nickname",
             "/api/v1/tools/**",
             "/api/v1/comments",
-            "/api/v1/users/kakao/login-url",
-            "/api/v1/tools/category"};
+            "/api/v1/users/kakao/login-url"};
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
