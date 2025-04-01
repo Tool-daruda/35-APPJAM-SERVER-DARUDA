ef61097968c7712b29c1109a0550c3bdd85c8f08
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-04-02 00:06:23 +0900

    #262 [refactor] `ImageController` Swagger 주석 추가 및 API 경로 수정

--- src/main/java/com/daruda/darudaserver/global/image/controller/ImageController.java
+++ src/main/java/com/daruda/darudaserver/global/image/controller/ImageController.java
@@ -13,21 +13,26 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.daruda.darudaserver.global.image.service.ImageService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/images")
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/image")
+@Tag(name = "image 컨트롤러", description = "이미지와 관련된 API를 처리합니다.")
 public class ImageController {
 	private final ImageService imageService;
 
 	@PostMapping
+	@Operation(summary = "이미지 업로드", description = "이미지를 S3 버킷에 업로드 합니다.")
 	public ResponseEntity<List<Long>> uploadImages(@RequestPart("files") List<MultipartFile> files) {
 		List<Long> imageUrls = imageService.uploadImages(files);
 		return ResponseEntity.ok(imageUrls);
 	}
 
 	@DeleteMapping
+	@Operation(summary = "이미지 삭제", description = "S3 버킷에 업로드된 이미지를 삭제 합니다.")
 	public ResponseEntity<List<Long>> deleteImages(@RequestBody List<Long> imageIds) {
 		imageService.deleteImages(imageIds);
 		return ResponseEntity.ok(imageIds);
