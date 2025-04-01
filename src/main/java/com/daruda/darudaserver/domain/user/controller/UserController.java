3df6ee40124338b794849fe380004ff7a0f5a49b
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-04-02 00:55:15 +0900

    #262 [refactor] HTTP 메서드 수정
    - `POST` -> `GET`

--- src/main/java/com/daruda/darudaserver/domain/user/controller/UserController.java
+++ src/main/java/com/daruda/darudaserver/domain/user/controller/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -83,7 +82,7 @@ public class UserController {
 	}
 
 	@DisableSwaggerSecurity
-	@PostMapping("/nickname")
+	@GetMapping("/nickname")
 	@Operation(summary = "닉네임 중복 확인", description = "닉네임 중복을 검사합니다.")
 	public ResponseEntity<ApiResponse<Boolean>> checkDuplicate(
 		@Parameter(description = "닉네임", example = "test")
