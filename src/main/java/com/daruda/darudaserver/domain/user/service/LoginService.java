3bf423f187236bdfa21f99ffac919ffb0abc666e
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-04-05 01:54:13 +0900

    #262 [refactor] 입력받은 소셜 로그인 타입에 따라 로그인 로직이 변경되도록 로직 수정

--- src/main/java/com/daruda/darudaserver/domain/user/service/LoginService.java
+++ src/main/java/com/daruda/darudaserver/domain/user/service/LoginService.java
@@ -7,7 +7,10 @@ import com.daruda.darudaserver.domain.user.entity.enums.SocialType;
 import com.daruda.darudaserver.global.error.code.ErrorCode;
 import com.daruda.darudaserver.global.error.exception.BadRequestException;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class LoginService {
 
 	@Value("${kakao.client_id}")
@@ -16,12 +19,22 @@ public class LoginService {
 	@Value("${kakao.redirect_uri}")
 	private String redirectUri;
 
+	private final KakaoService kakaoService;
+
 	public String getLoginUrl(SocialType socialType) {
 		return switch (socialType) {
 			case KAKAO ->
 				"https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=" + clientId + "&redirect_uri="
 					+ redirectUri;
 			// case GOOGLE -> "google redirect url";
+			default -> throw new BadRequestException(ErrorCode.SOCIAL_TYPE_BAD_REQUEST);
+		};
+	}
+
+	public SocialService findSocialService(SocialType socialType) {
+		return switch (socialType) {
+			case KAKAO -> kakaoService;
+			// case GOOGLE -> googleSocialService;
 			default -> throw new BadRequestException(ErrorCode.SOCIAL_TYPE_BAD_REQUEST);
 		};
 	}
