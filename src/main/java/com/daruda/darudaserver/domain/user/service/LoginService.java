e827793a461a0e0f2d9cd06f44efe5961e58fe1e
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-04-05 01:40:28 +0900

    #262 [feat] 소셜 로그인 redirect URL 생성 로직 구현

---
+++ src/main/java/com/daruda/darudaserver/domain/user/service/LoginService.java
@@ -0,0 +1,28 @@
+package com.daruda.darudaserver.domain.user.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.daruda.darudaserver.domain.user.entity.enums.SocialType;
+import com.daruda.darudaserver.global.error.code.ErrorCode;
+import com.daruda.darudaserver.global.error.exception.BadRequestException;
+
+@Service
+public class LoginService {
+
+	@Value("${kakao.client_id}")
+	private String clientId;
+
+	@Value("${kakao.redirect_uri}")
+	private String redirectUri;
+
+	public String getLoginUrl(SocialType socialType) {
+		return switch (socialType) {
+			case KAKAO ->
+				"https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=" + clientId + "&redirect_uri="
+					+ redirectUri;
+			// case GOOGLE -> "google redirect url";
+			default -> throw new BadRequestException(ErrorCode.SOCIAL_TYPE_BAD_REQUEST);
+		};
+	}
+}
