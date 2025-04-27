8fc5f2f26fc065709130ca55ece7e81f56aac378
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-04-28 01:12:23 +0900

    #270 style: 코드 정렬

--- src/main/java/com/daruda/darudaserver/domain/user/dto/response/kakao/KakaoAccount.java
+++ src/main/java/com/daruda/darudaserver/domain/user/dto/response/kakao/KakaoAccount.java
@@ -5,5 +5,8 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public record KakaoAccount(@JsonProperty("profile") KakaoUserProfile kakaoUserProfile, String email) {
+public record KakaoAccount(
+	@JsonProperty("profile") KakaoUserProfile kakaoUserProfile,
+	String email
+) {
 }
