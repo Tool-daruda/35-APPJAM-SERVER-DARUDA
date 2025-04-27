36fe261d234eaa691e442b99432f127e96f22b2e
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-04-27 21:09:43 +0900

    #270 refactor: `Enum`을 `String`으로 변환하지 않고 반환하도록 변경

--- src/main/java/com/daruda/darudaserver/domain/user/dto/response/UpdateMyResponse.java
+++ src/main/java/com/daruda/darudaserver/domain/user/dto/response/UpdateMyResponse.java
@@ -1,13 +1,15 @@
 package com.daruda.darudaserver.domain.user.dto.response;
 
+import com.daruda.darudaserver.domain.user.entity.enums.Positions;
+
 import lombok.Builder;
 
 @Builder
 public record UpdateMyResponse(
 	String nickname,
-	String positions
+	Positions positions
 ) {
-	public static UpdateMyResponse of(String nickname, String positions) {
+	public static UpdateMyResponse of(String nickname, Positions positions) {
 		return UpdateMyResponse.builder()
 			.nickname(nickname)
 			.positions(positions)
