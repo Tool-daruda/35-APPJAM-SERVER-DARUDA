98014177f798de777e40c272f18182400d1fcb31
woals2840 <woals2840@gmail.com>
2025-05-08 11:41:08 +0900

    #269 [fix] yml파일 못 읽어들임 문제 해결

--- src/main/java/com/daruda/darudaserver/DarudaApplication.java
+++ src/main/java/com/daruda/darudaserver/DarudaApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import io.awspring.cloud.autoconfigure.s3.S3AutoConfiguration;
+
 @EnableFeignClients
-@SpringBootApplication
 @EnableWebMvc
+@SpringBootApplication(exclude = {S3AutoConfiguration.class})
 public class DarudaApplication {
 
 	public static void main(String[] args) {
