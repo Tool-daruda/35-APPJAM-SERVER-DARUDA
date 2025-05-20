4ed3796c5f529f0ce6efefeb585ef567da8ca199
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-05-20 21:23:04 +0900

    #274 feat: 알림 기능을 위한 DTO 추가

---
+++ src/main/java/com/daruda/darudaserver/domain/notification/dto/request/NoticeRequest.java
@@ -0,0 +1,7 @@
+package com.daruda.darudaserver.domain.notification.dto.request;
+
+public record NoticeRequest(
+	String title,
+	String content
+) {
+}
---
+++ src/main/java/com/daruda/darudaserver/domain/notification/dto/response/NotificationResponse.java
@@ -0,0 +1,31 @@
+package com.daruda.darudaserver.domain.notification.dto.response;
+
+import java.sql.Timestamp;
+
+import com.daruda.darudaserver.domain.notification.entity.NotificationEntity;
+import com.daruda.darudaserver.domain.notification.entity.enums.NotificationType;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public record NotificationResponse(
+	Long id,
+	String title,
+	String content,
+	Long boardId,
+	NotificationType type,
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd", timezone = "Asia/Seoul")
+	Timestamp createdAt,
+	boolean isRead
+) {
+	public static NotificationResponse from(NotificationEntity notificationEntity) {
+		return new NotificationResponse(
+			notificationEntity.getId(),
+			notificationEntity.getTitle(),
+			notificationEntity.getContent(),
+			notificationEntity.getComment() != null ?
+				notificationEntity.getComment().getBoard().getId() : null,
+			notificationEntity.getType(),
+			notificationEntity.getCreatedAt(),
+			notificationEntity.isRead()
+		);
+	}
+}
