429d74e43ffe41852cac85c43b42ca7d8c8f67a1
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-05-21 00:26:22 +0900

    #274 refactor: 필드 유효성 검증 로직 추가

--- src/main/java/com/daruda/darudaserver/domain/notification/dto/request/NoticeRequest.java
+++ src/main/java/com/daruda/darudaserver/domain/notification/dto/request/NoticeRequest.java
@@ -1,7 +1,15 @@
 package com.daruda.darudaserver.domain.notification.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record NoticeRequest(
+	@NotBlank(message = "제목은 필수 입력값입니다")
+	@Size(max = 100, message = "제목은 100자 이내로 입력해주세요")
 	String title,
+
+	@NotBlank(message = "내용은 필수 입력값입니다")
+	@Size(max = 1000, message = "내용은 1000자 이내로 입력해주세요")
 	String content
 ) {
 }
