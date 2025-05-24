c9cd74db119f90885e2d04bb2bfa6b3f00cb27b3
노수인 <bigpie1367@gmail.com>
2025-05-24 17:13:32 +0900

    [FEAT] 신고 유형에 따른 entity 정의 #277

---
+++ src/main/java/com/daruda/darudaserver/domain/report/entity/ReportStatus.java
@@ -0,0 +1,15 @@
+package com.daruda.darudaserver.domain.report.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ReportStatus {
+    PENDING("처리 대기중"),
+    PROCESSING("처리중"),
+    COMPLETED("처리 완료"),
+    REJECTED("신고 거절");
+
+    private final String description;
+} 
