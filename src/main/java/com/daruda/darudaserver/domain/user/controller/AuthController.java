763785a0fa126bf9df34149f4507867e8538391f
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-06-06 16:33:45 +0900

    #281 [refactor] `RefreshToken`을 `Cookie`에 저장하도록 변경

--- src/main/java/com/daruda/darudaserver/domain/user/controller/AuthController.java
+++ src/main/java/com/daruda/darudaserver/domain/user/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.daruda.darudaserver.domain.user.controller;
 
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,11 +13,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.daruda.darudaserver.domain.user.dto.request.LoginRequest;
-import com.daruda.darudaserver.domain.user.dto.request.ReissueTokenRequest;
 import com.daruda.darudaserver.domain.user.dto.request.SignUpRequest;
 import com.daruda.darudaserver.domain.user.dto.response.JwtTokenResponse;
 import com.daruda.darudaserver.domain.user.dto.response.LoginResponse;
+import com.daruda.darudaserver.domain.user.dto.response.LoginSuccessResponse;
 import com.daruda.darudaserver.domain.user.dto.response.SignUpSuccessResponse;
+import com.daruda.darudaserver.domain.user.dto.response.TokenResponse;
 import com.daruda.darudaserver.domain.user.dto.response.UserInformationResponse;
 import com.daruda.darudaserver.domain.user.entity.enums.SocialType;
 import com.daruda.darudaserver.domain.user.service.AuthService;
@@ -28,6 +31,7 @@ import com.daruda.darudaserver.global.error.code.SuccessCode;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -39,6 +43,8 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class AuthController {
 
+	private static final int COOKIE_MAX_AGE = 7 * 24 * 60 * 60;
+	private static final String REFRESH_TOKEN = "refreshToken";
 	private final AuthService authService;
 	private final TokenService tokenService;
 
@@ -47,7 +53,8 @@ public class AuthController {
 	@Operation(summary = "소셜 로그인 URL 반환", description = "소셜 로그인 URL을 반환합니다.")
 	public ResponseEntity<ApiResponse<String>> requestLoginUrl(
 		@Parameter(description = "소셜 로그인 타입", example = "KAKAO")
-		@RequestParam SocialType socialType) {
+		@RequestParam SocialType socialType
+	) {
 		SocialService socialService = authService.findSocialService(socialType);
 		String redirectUrl = socialService.getLoginUrl();
 		return ResponseEntity.ok(ApiResponse.ofSuccessWithData(redirectUrl, SuccessCode.SUCCESS_REDIRECT));
@@ -60,11 +67,28 @@ public class AuthController {
 	public ResponseEntity<ApiResponse<LoginResponse>> login(
 		@Parameter(description = "Authorization Code", example = "1234")
 		@RequestParam("code") String code,
-		@RequestBody LoginRequest loginRequest) {
+		@RequestBody LoginRequest loginRequest,
+		HttpServletResponse httpServletResponse
+	) {
 		log.debug("소셜 로그인 요청 수신 (socialType = {})", loginRequest.socialType());
 		SocialService socialService = authService.findSocialService(loginRequest.socialType());
 		UserInformationResponse userInformationResponse = socialService.getInfo(code);
-		LoginResponse loginResponse = authService.login(userInformationResponse);
+		LoginSuccessResponse loginSuccessResponse = authService.login(userInformationResponse);
+
+		ResponseCookie cookie = ResponseCookie.from(REFRESH_TOKEN,
+				loginSuccessResponse.jwtTokenResponse().refreshToken())
+			.maxAge(COOKIE_MAX_AGE)
+			.path("/")
+			.secure(true)
+			.sameSite("None")
+			.httpOnly(true)
+			.build();
+
+		httpServletResponse.setHeader("Set-Cookie", cookie.toString());
+
+		LoginResponse loginResponse = LoginResponse.of(loginSuccessResponse.email(), loginSuccessResponse.isUser(),
+			loginSuccessResponse.nickname(), loginSuccessResponse.jwtTokenResponse().accessToken());
+
 		return ResponseEntity.ok(ApiResponse.ofSuccessWithData(loginResponse, SuccessCode.SUCCESS_LOGIN));
 	}
 
@@ -72,7 +96,8 @@ public class AuthController {
 	@PostMapping("/sign-up")
 	@Operation(summary = "회원 가입", description = "회원 가입을 진행합니다.")
 	public ResponseEntity<ApiResponse<SignUpSuccessResponse>> register(
-		@Valid @RequestBody SignUpRequest signUpRequest) {
+		@Valid @RequestBody SignUpRequest signUpRequest
+	) {
 		SignUpSuccessResponse signUpSuccessResponse = authService.register(signUpRequest.email(),
 			signUpRequest.nickname(), signUpRequest.positions());
 		return ResponseEntity.ok(ApiResponse.ofSuccessWithData(signUpSuccessResponse, SuccessCode.SUCCESS_CREATE));
@@ -80,21 +105,41 @@ public class AuthController {
 
 	@PostMapping("/logout")
 	@Operation(summary = "로그아웃", description = "로그아웃을 진행합니다.")
-	public ResponseEntity<ApiResponse<Long>> logout(@AuthenticationPrincipal Long userId) {
+	public ResponseEntity<ApiResponse<Long>> logout(
+		@AuthenticationPrincipal Long userId
+	) {
 		return ResponseEntity.ok(ApiResponse.ofSuccessWithData(authService.logout(userId), SuccessCode.SUCCESS_LOGOUT));
 	}
 
 	@DisableSwaggerSecurity
 	@PostMapping("/reissue")
 	@Operation(summary = "Access Token 재발급", description = "Refresh Token을 통해 Access Token을 재발급합니다.")
-	public ResponseEntity<ApiResponse<JwtTokenResponse>> reissueToken(@RequestBody ReissueTokenRequest request) {
-		return ResponseEntity.ok(ApiResponse.ofSuccessWithData(tokenService.reissueToken(request.refreshToken()),
-			SuccessCode.SUCCESS_REISSUE));
+	public ResponseEntity<ApiResponse<TokenResponse>> reissueToken(
+		@CookieValue(value = REFRESH_TOKEN) final String refreshToken,
+		HttpServletResponse httpServletResponse
+	) {
+		JwtTokenResponse tokenResponse = tokenService.reissueToken(refreshToken);
+
+		ResponseCookie cookie = ResponseCookie.from(REFRESH_TOKEN, tokenResponse.refreshToken())
+			.maxAge(COOKIE_MAX_AGE)
+			.path("/")
+			.secure(true)
+			.sameSite("None")
+			.httpOnly(true)
+			.build();
+
+		httpServletResponse.setHeader("Set-Cookie", cookie.toString());
+
+		TokenResponse response = TokenResponse.from(tokenResponse.accessToken());
+
+		return ResponseEntity.ok(ApiResponse.ofSuccessWithData(response, SuccessCode.SUCCESS_REISSUE));
 	}
 
 	@DeleteMapping("/withdraw")
 	@Operation(summary = "회원 탈퇴", description = "회원 탈퇴를 진행합니다.")
-	public ResponseEntity<ApiResponse<Void>> withdraw(@AuthenticationPrincipal Long userId) {
+	public ResponseEntity<ApiResponse<Void>> withdraw(
+		@AuthenticationPrincipal Long userId
+	) {
 		authService.withdraw(userId);
 		return ResponseEntity.ok(ApiResponse.ofSuccess(SuccessCode.SUCCESS_WITHDRAW));
 	}
