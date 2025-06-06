a5bf96b2e9578279e5562ce3bba30885afcc0c5a
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-06-06 16:28:34 +0900

    #281 [refactor] 로그인 `DTO`와 로그인 로직 성공 `DTO` 분리

--- src/main/java/com/daruda/darudaserver/domain/user/dto/response/LoginResponse.java
+++ src/main/java/com/daruda/darudaserver/domain/user/dto/response/LoginResponse.java
@@ -2,20 +2,22 @@ package com.daruda.darudaserver.domain.user.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import lombok.Builder;
+
+@Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record LoginResponse(
 	String email,
 	boolean isUser,
 	String nickname,
-	JwtTokenResponse jwtTokenResponse
+	String accessToken
 ) {
-	//등록된 회원이 아닌 경우
-	public static LoginResponse ofNonRegisteredUser(String email) {
-		return new LoginResponse(email, false, null, null);
-	}
-
-	//등록된 회원인 경우
-	public static LoginResponse ofRegisteredUser(JwtTokenResponse jwtTokenResponse, String nickname) {
-		return new LoginResponse(null, true, nickname, jwtTokenResponse);
+	public static LoginResponse of(String email, boolean isUser, String nickname, String accessToken) {
+		return LoginResponse.builder()
+			.email(email)
+			.isUser(isUser)
+			.nickname(nickname)
+			.accessToken(accessToken)
+			.build();
 	}
 }
