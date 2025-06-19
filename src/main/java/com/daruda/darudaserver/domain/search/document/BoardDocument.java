d232cbcd53bc4ba9da520649f582ca46ba8d466c
woals2840 <woals2840@gmail.com>
2025-06-20 00:10:41 +0900

    #275 [feat] Document 생성

--- src/main/java/com/daruda/darudaserver/domain/search/document/BoardDocument.java
+++ src/main/java/com/daruda/darudaserver/domain/search/document/BoardDocument.java
@@ -1,14 +1,15 @@
 package com.daruda.darudaserver.domain.search.document;
 
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 import org.springframework.data.elasticsearch.annotations.Setting;
 
 import com.daruda.darudaserver.domain.community.entity.Board;
-import com.daruda.darudaserver.global.image.entity.Image;
 
-import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,12 +20,12 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-@Setting(settingPath = "src/main/resources/elasticsearch/custom-settings.json")
+@Setting(settingPath = "/elasticsearch/custom-settings.json")
 @Document(indexName = "board")
 public class BoardDocument {
 	@Id
 	@Field(type = FieldType.Keyword)
-	private Long id;
+	private String id;
 
 	@Field(type = FieldType.Text, analyzer = "custom_analyzer")
 	private String title;
@@ -32,7 +33,7 @@ public class BoardDocument {
 	@Field(type = FieldType.Text, analyzer = "custom_analyzer")
 	private String content;
 
-	@Field(type = FieldType.Keyword, analyzer = "custom_analyzer")
+	@Field(type = FieldType.Text, analyzer = "custom_analyzer")
 	private String toolName;
 
 	@Field(type = FieldType.Keyword)
@@ -42,17 +43,17 @@ public class BoardDocument {
 	private Long toolId;
 
 	@Field(type = FieldType.Keyword)
-	private String imageUrl;
+	private List<String> imageUrl;
 
-	public static BoardDocument from(Board board, Image image) {
+	public static BoardDocument from(Board board, List<String> imageUrls) {
 		return BoardDocument.builder()
 			.content(board.getContent())
-			.id(board.getId())
+			.id(board.getId().toString())
 			.title(board.getTitle())
-			.toolId(board.getTool().getToolId())
-			.toolName(board.getTool().getToolMainName())
+			.toolId(board.getTool() != null ? board.getTool().getToolId() : null)
+			.toolName(board.getTool() != null ? board.getTool().getToolMainName() : null)
 			.createdAt(board.getCreatedAt().toString())
-			.imageUrl(image.getImageUrl())
+			.imageUrl(imageUrls)
 			.build();
 	}
 }
---
+++ src/main/java/com/daruda/darudaserver/domain/search/document/ToolDocument.java
@@ -0,0 +1,86 @@
+package com.daruda.darudaserver.domain.search.document;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.Setting;
+
+import com.daruda.darudaserver.domain.tool.entity.Tool;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Setting(settingPath = "/elasticsearch/custom-settings.json")
+@Document(indexName = "tool")
+public class ToolDocument {
+
+	@Id
+	@Field(type = FieldType.Keyword)
+	private String id;
+
+	@Field(type = FieldType.Text, analyzer = "custom_analyzer")
+	private String toolMainName;
+
+	@Field(type = FieldType.Text, analyzer = "custom_analyzer")
+	private String toolSubName;
+
+	@Field(type = FieldType.Keyword)
+	private String category;
+
+	@Field(type = FieldType.Text)
+	private String toolLink;
+
+	@Field(type = FieldType.Text, analyzer = "custom_analyzer")
+	private String description;
+
+	@Field(type = FieldType.Keyword)
+	private String license;
+
+	@Field(type = FieldType.Boolean)
+	private Boolean supportKorea;
+
+	@Field(type = FieldType.Text, analyzer = "custom_analyzer")
+	private String detailDescription;
+
+	@Field(type = FieldType.Text)
+	private String planLink;
+
+	@Field(type = FieldType.Keyword)
+	private String bgColor;
+
+	@Field(type = FieldType.Boolean)
+	private boolean fontColor;
+
+	@Field(type = FieldType.Integer)
+	private int viewCount;
+
+	@Field(type = FieldType.Integer)
+	private int popular;
+
+	public static ToolDocument from(Tool tool) {
+		return ToolDocument.builder()
+			.id(tool.getToolId().toString())
+			.toolMainName(tool.getToolMainName())
+			.toolSubName(tool.getToolSubName())
+			.category(tool.getCategory().name())
+			.toolLink(tool.getToolLink())
+			.description(tool.getDescription())
+			.license(tool.getLicense().name())
+			.supportKorea(tool.getSupportKorea())
+			.detailDescription(tool.getDetailDescription())
+			.planLink(tool.getPlanLink())
+			.bgColor(tool.getBgColor())
+			.fontColor(tool.isFontColor())
+			.viewCount(tool.getViewCount())
+			.popular(tool.getPopular())
+			.build();
+	}
+}
