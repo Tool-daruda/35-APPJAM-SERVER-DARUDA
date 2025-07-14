5de1e2ceda86a26babdd2cc8b376d077d81613a3
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-07-15 02:07:46 +0900

    #300 [refactor]: `/presigned-url` API 요청 방식 변경
    - `body` -> `parameter`

--- src/main/java/com/daruda/darudaserver/global/image/controller/ImageController.java
+++ src/main/java/com/daruda/darudaserver/global/image/controller/ImageController.java
@@ -8,19 +8,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.daruda.darudaserver.global.error.code.SuccessCode;
 import com.daruda.darudaserver.global.error.dto.SuccessResponse;
-import com.daruda.darudaserver.global.image.dto.request.GetPresignedUrlRequest;
-import com.daruda.darudaserver.global.image.dto.response.GetPresignedUrlListResponse;
 import com.daruda.darudaserver.global.image.service.ImageService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -46,12 +44,11 @@ public class ImageController {
 
 	@GetMapping("/presigned-url")
 	@Operation(summary = "이미지 업로드 용 presigned-url발급", description = "이미지 업로드를 위한 presignedUrl을 발급합니다")
-	public ResponseEntity<SuccessResponse<?>> getPresignedUrl(
-		@Valid @RequestBody GetPresignedUrlRequest getPresignedUrlRequest) {
-		GetPresignedUrlListResponse getPresignedUrlResponseList = imageService.getUploadPresignedUrl(
-			getPresignedUrlRequest);
+	public ResponseEntity<SuccessResponse<String>> getPresignedUrl(
+		@RequestParam String imageName) {
+		String presignedUrl = imageService.createUploadPresignedUrl(imageName);
 
-		return ResponseEntity.ok(SuccessResponse.of(SuccessCode.SUCCESS_CREATE, getPresignedUrlResponseList));
+		return ResponseEntity.ok(SuccessResponse.of(SuccessCode.SUCCESS_CREATE, presignedUrl));
 	}
 
 }
