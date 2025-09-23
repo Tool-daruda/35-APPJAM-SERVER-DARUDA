ddd243f1ca066cfab318ded4f6d2b7a53dbf377a
woals2840 <woals2840@gmail.com>
2025-09-24 00:25:36 +0900

    #317 [feat] dto 정의

---
+++ src/main/java/com/daruda/darudaserver/domain/admin/dto/request/CreateToolRequest.java
@@ -0,0 +1,66 @@
+package com.daruda.darudaserver.domain.admin.dto.request;
+
+import java.util.List;
+
+import com.daruda.darudaserver.domain.tool.entity.Plan;
+import com.daruda.darudaserver.domain.tool.entity.ToolCore;
+import com.daruda.darudaserver.domain.tool.entity.ToolPlatForm;
+import com.daruda.darudaserver.global.image.entity.Image;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record CreateToolRequest(
+	String toolMainName,
+	String toolSubName,
+	String category,
+	String toolLink,
+	String description,
+	String license,
+	boolean supportKorea,
+	String detailDescription,
+	String planLink,
+	String bgColor,
+	boolean fontColor,
+	String toolLogo,
+	ToolPlatForm platform,
+	List<String> keywords,
+	List<ToolCore> cores,
+	List<Plan> plans,
+	List<String> images,
+	List<String> videos,
+	List<Integer> relatedToolIds
+) {
+	@JsonIgnoreProperties(ignoreUnknown = true)
+	public record ToolPlatform(
+		boolean web,
+		boolean windows,
+		boolean mac
+	) {}
+
+	@JsonIgnoreProperties(ignoreUnknown = true)
+	public record ToolCore(
+		String coreTitle,
+		String coreContent
+	) {}
+
+	@JsonIgnoreProperties(ignoreUnknown = true)
+	public record ToolPlan(
+		String planName,
+		int monthlyPrice,
+		int annualPrice,
+		String description,
+		boolean isDollar
+	) {}
+
+	@JsonIgnoreProperties(ignoreUnknown = true)
+	public record Image(
+		String imageUrl
+	) {}
+
+	@JsonIgnoreProperties(ignoreUnknown = true)
+	public record Video(
+		String videoUrl
+	) {}
+}
