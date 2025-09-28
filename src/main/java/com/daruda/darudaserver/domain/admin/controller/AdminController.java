7569c237802cff06c44c3f6b1a909e1a523b8ef6
노수인 <bigpie1367@gmail.com>
2025-09-28 18:31:32 +0900

    Feat: 관리자용 툴 업데이트 컨트롤러 구성 #316

--- src/main/java/com/daruda/darudaserver/domain/admin/controller/AdminController.java
+++ src/main/java/com/daruda/darudaserver/domain/admin/controller/AdminController.java
@@ -2,6 +2,8 @@ package com.daruda.darudaserver.domain.admin.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.daruda.darudaserver.domain.admin.dto.request.CreateToolRequest;
 import com.daruda.darudaserver.domain.admin.service.AdminService;
+import com.daruda.darudaserver.domain.admin.dto.request.UpdateToolRequest;
 import com.daruda.darudaserver.domain.community.dto.req.BoardCreateAndUpdateReq;
 import com.daruda.darudaserver.domain.community.dto.res.BoardRes;
 import com.daruda.darudaserver.global.common.response.ApiResponse;
@@ -37,6 +40,17 @@ public class AdminController {
 
 		adminService.createTool(createToolRequest);
 		return ResponseEntity.ok(SuccessResponse.of(SuccessCode.SUCCESS_CREATE));
+	}
+
+	@PatchMapping("/tools/{toolId}")
+	@Operation(summary = "관리자용 툴 수정", description = "관리자용 툴 수정 API입니다.")
+    public ResponseEntity<SuccessResponse<?>> updateTool(
+		@AuthenticationPrincipal Long userId,
+		@Parameter(description = "수정할 툴 ID") @PathVariable Long toolId,
+        @Parameter(description = "수정 요청 DTO") @RequestBody @Valid UpdateToolRequest request
+    ) {
+		adminService.updateTool(toolId, request);
+		return ResponseEntity.ok(SuccessResponse.of(SuccessCode.SUCCESS_UPDATE));
 	}
 
 }
