5240952c949dcfe6dbcd0d2d6af7dddee259ab5c
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-10-16 01:33:33 +0900

    #323 [feat] `deleteByTool` JPA 메서드 구현

--- src/main/java/com/daruda/darudaserver/domain/tool/repository/PlanRepository.java
+++ src/main/java/com/daruda/darudaserver/domain/tool/repository/PlanRepository.java
@@ -3,13 +3,20 @@ package com.daruda.darudaserver.domain.tool.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
 import com.daruda.darudaserver.domain.tool.entity.Plan;
 import com.daruda.darudaserver.domain.tool.entity.Tool;
 
+import jakarta.transaction.Transactional;
+
 @Repository
 public interface PlanRepository extends JpaRepository<Plan, Long> {
 
 	List<Plan> findAllByTool(Tool tool);
+
+	@Modifying(clearAutomatically = true, flushAutomatically = true)
+	@Transactional
+	void deleteByTool(Tool tool);
 }
--- src/main/java/com/daruda/darudaserver/domain/tool/repository/ToolVideoRepository.java
+++ src/main/java/com/daruda/darudaserver/domain/tool/repository/ToolVideoRepository.java
@@ -3,12 +3,19 @@ package com.daruda.darudaserver.domain.tool.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
 import com.daruda.darudaserver.domain.tool.entity.Tool;
 import com.daruda.darudaserver.domain.tool.entity.ToolVideo;
 
+import jakarta.transaction.Transactional;
+
 @Repository
 public interface ToolVideoRepository extends JpaRepository<ToolVideo, Long> {
 	List<ToolVideo> findAllByTool(final Tool toolId);
+
+	@Modifying(clearAutomatically = true, flushAutomatically = true)
+	@Transactional
+	void deleteByTool(Tool tool);
 }
