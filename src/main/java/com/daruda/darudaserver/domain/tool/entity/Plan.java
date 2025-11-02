d2562340b01d2baa20a6d0c1a5e6881a37537779
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-11-03 04:40:29 +0900

    #327 [refactor] 요구사항 변경에 따른 툴 엔티티 재설개

--- src/main/java/com/daruda/darudaserver/domain/tool/entity/Plan.java
+++ src/main/java/com/daruda/darudaserver/domain/tool/entity/Plan.java
@@ -25,7 +25,6 @@ import lombok.NoArgsConstructor;
 @Table(name = "tool_plan")
 public class Plan {
 
-	// TODO: 재구조화 필요 (연간, 월간, isDollar 정책 확립)
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long planId;
@@ -33,17 +32,11 @@ public class Plan {
 	@Column(name = "plan_name", nullable = false)
 	private String planName;
 
-	@Column(name = "price_monthly", nullable = false)
-	private Long priceMonthly;
-
-	@Column(name = "price_annual")
-	private Long priceAnnual;
+	@Column(name = "price", nullable = false)
+	private Long price;
 
 	@Column(name = "description", nullable = false, length = 500)
 	private String description;
-
-	@Column(name = "is_dollar", nullable = false)
-	private Boolean isDollar;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "tool_id", nullable = false)
@@ -52,9 +45,8 @@ public class Plan {
 	public static Plan create(CreateToolPlanRequest toolPlanRequest, Tool tool) {
 		return Plan.builder()
 			.planName(toolPlanRequest.planName())
-			.priceMonthly(toolPlanRequest.planPrice())
+			.price(toolPlanRequest.planPrice())
 			.description(toolPlanRequest.planDescription())
-			.isDollar(false)
 			.tool(tool)
 			.build();
 	}
--- src/main/java/com/daruda/darudaserver/domain/tool/entity/Tool.java
+++ src/main/java/com/daruda/darudaserver/domain/tool/entity/Tool.java
@@ -30,59 +30,45 @@ import lombok.NoArgsConstructor;
 @Builder
 public class Tool {
 
+	@Column(name = "plan_type")
+	PlanType planType;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long toolId;
-
 	@Column(name = "tool_main_name", nullable = false)
 	private String toolMainName;
-
 	@Column(name = "tool_sub_name", nullable = false)
 	private String toolSubName;
-
 	@Enumerated(EnumType.STRING)
 	@Column(name = "category", nullable = false)
 	private Category category;
-
 	@Column(name = "tool_link", nullable = false, length = 5000)
 	private String toolLink;
-
 	@Column(name = "description", nullable = false, length = 500)
 	private String description;
-
 	@Enumerated(EnumType.STRING)
 	@Column(name = "license", nullable = false)
 	private License license;
-
 	@Column(name = "support_korea", nullable = false)
 	private Boolean supportKorea;
-
 	@Column(name = "detail_description", nullable = false, length = 500)
 	private String detailDescription;
-
 	@Column(name = "plan_link", length = 5000)
 	private String planLink;
-
 	@Column(name = "bg_color", nullable = false)
 	private String bgColor;
-
 	@Column(name = "font_color")
 	private boolean fontColor;
-
 	@Column(name = "tool_logo", nullable = false)
 	private String toolLogo;
-
 	@CreatedDate
 	@Column(name = "created_at", nullable = false, updatable = false)
 	private Timestamp createdAt;
-
 	@LastModifiedDate
 	@Column(name = "updated_at")
 	private Timestamp updatedAt;
-
 	@Column(columnDefinition = "integer default 0", nullable = false)
 	private int viewCount;
-
 	@Column(name = "popular", columnDefinition = "integer default 0")
 	private int popular;
 
