7198eb99bdf73b88bd04990f64b50ec48db9f683
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-11-03 21:10:53 +0900

    #327 [refactor] enum값 수정

--- src/main/java/com/daruda/darudaserver/domain/tool/entity/License.java
+++ src/main/java/com/daruda/darudaserver/domain/tool/entity/License.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 @Getter
 public enum License {
 	FREE("무료"),
-	PARTIALLY_FREE("부분 무료"),
+	PARTIALLY_FREE("부분 유료"),
 	PAID("유료");
 
 	private final String koreanName;
@@ -16,11 +16,6 @@ public enum License {
 	// 생성자
 	License(String koreanName) {
 		this.koreanName = koreanName;
-	}
-
-	// Getter 메서드
-	public String getKoreanName() {
-		return koreanName;
 	}
 
 	public static License from(String koreanName) {
@@ -33,5 +28,10 @@ public enum License {
 			}
 		}
 		throw new NotFoundException(ErrorCode.DATA_NOT_FOUND);
+	}
+
+	// Getter 메서드
+	public String getKoreanName() {
+		return koreanName;
 	}
 }
