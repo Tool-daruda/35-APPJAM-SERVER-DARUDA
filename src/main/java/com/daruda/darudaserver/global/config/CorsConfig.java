c9234f2e4b1881c48ea5e984b8237bd5947c6cb3
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2025-11-14 14:44:48 +0900

    #334 [chore] 관리자 페이지 CORS 설정 추가

--- src/main/java/com/daruda/darudaserver/global/config/CorsConfig.java
+++ src/main/java/com/daruda/darudaserver/global/config/CorsConfig.java
@@ -2,6 +2,7 @@ package com.daruda.darudaserver.global.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -11,6 +12,9 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CorsConfig {
+
+	@Value("${admin.page.origin}")
+	private static String adminPageOrigin;
 
 	public static CorsConfigurationSource configurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
@@ -23,7 +27,8 @@ public class CorsConfig {
 			"https://www.daruda.shop",
 			"https://daruda.site",
 			"https://api.daruda.site",
-			"https://www.daruda.site"
+			"https://www.daruda.site",
+			adminPageOrigin
 		);
 		configuration.setAllowedOriginPatterns(allowedOriginPatterns);
 
