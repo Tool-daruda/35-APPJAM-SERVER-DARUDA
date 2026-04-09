089e6efaf93552945c56580017153ef690762b4d
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2026-04-09 19:01:17 +0900

    refactor: 활동 제한 알림 본문을 텍스트 블록으로 수정

--- src/main/java/com/daruda/darudaserver/domain/notification/entity/enums/NotificationFormat.java
+++ src/main/java/com/daruda/darudaserver/domain/notification/entity/enums/NotificationFormat.java
@@ -18,11 +18,12 @@ public enum NotificationFormat {
 
 	// 서비스 제한 알림 형식
 	COMMUNITY_BLOCK_NOTICE_TITLE("[공지] %s님의 신고가 접수되었습니다."),
-	COMMUNITY_BLOCK_NOTICE_CONTENT("회원님께서는 \n"
-		+ "커뮤니티이용규칙을 위반하여\n"
-		+ "%s부로 %d일간 커뮤니티의 일부 활동을 제한합니다. \n"
-		+ "문의사항이 있다면 홈 화면 우측 상단의 ‘문의하기’를 이용해 주시기 바랍니다.\n"
-		+ "감사합니다.");
+	COMMUNITY_BLOCK_NOTICE_CONTENT("""
+		회원님께서는\s
+		커뮤니티이용규칙을 위반하여
+		%s부로 %d일간 커뮤니티의 일부 활동을 제한합니다.\s
+		문의사항이 있다면 홈 화면 우측 상단의 ‘문의하기’를 이용해 주시기 바랍니다.
+		감사합니다.""");
 
 	private final String messageFormat;
 
