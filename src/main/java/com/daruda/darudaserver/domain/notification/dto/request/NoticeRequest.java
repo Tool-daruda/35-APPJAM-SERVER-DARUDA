52fa84f2942d8df988b8059fd2602f771d4eb048
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2026-04-09 18:40:10 +0900

    refactor: 요구사항 변경에 따른 공지 DTO 수정
    - 공지 내용 대신, Notion URL 추가

--- src/main/java/com/daruda/darudaserver/domain/notification/dto/request/NoticeRequest.java
+++ src/main/java/com/daruda/darudaserver/domain/notification/dto/request/NoticeRequest.java
@@ -8,8 +8,7 @@ public record NoticeRequest(
 	@Size(max = 100, message = "제목은 100자 이내로 입력해주세요")
 	String title,
 
-	@NotBlank(message = "내용은 필수 입력값입니다")
-	@Size(max = 1000, message = "내용은 1000자 이내로 입력해주세요")
-	String content
+	@NotBlank(message = "URL은 필수 입력값입니다")
+	String url
 ) {
 }
