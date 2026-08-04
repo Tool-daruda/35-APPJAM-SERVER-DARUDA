5d2eb468b362069112639c7bf9c297d11aabcbba
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2026-08-04 20:10:48 +0900

    #361 [refactor] jakarta → Spring `@Transactional` import 교체

--- src/main/java/com/daruda/darudaserver/domain/tool/repository/RelatedToolRepository.java
+++ src/main/java/com/daruda/darudaserver/domain/tool/repository/RelatedToolRepository.java
@@ -5,11 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.daruda.darudaserver.domain.tool.entity.RelatedTool;
 import com.daruda.darudaserver.domain.tool.entity.Tool;
-
-import jakarta.transaction.Transactional;
 
 @Repository
 public interface RelatedToolRepository extends JpaRepository<RelatedTool, Long> {
