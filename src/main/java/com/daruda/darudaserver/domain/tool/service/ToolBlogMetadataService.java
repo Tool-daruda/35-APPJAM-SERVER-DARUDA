685425e5ac297422ac63caa9b14abe37c27dc18c
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2026-08-29 12:38:08 +0900

    #363 [refactor] `ToolBlogMetadataService`에 클래스 레벨 읽기 전용 트랜잭션 선언

--- src/main/java/com/daruda/darudaserver/domain/tool/service/ToolBlogMetadataService.java
+++ src/main/java/com/daruda/darudaserver/domain/tool/service/ToolBlogMetadataService.java
@@ -22,6 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ToolBlogMetadataService {
 
 	private final ToolBlogRepository toolBlogRepository;
