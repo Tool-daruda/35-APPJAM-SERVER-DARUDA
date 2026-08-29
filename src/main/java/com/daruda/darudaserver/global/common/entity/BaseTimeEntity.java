1cae9f419b200573435d60efd7d05e749ee57fa1
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2026-08-29 16:58:33 +0900

    #377 [refactor] 미사용 JPA Auditing 설정 및 엔티티 리스너 제거

--- src/main/java/com/daruda/darudaserver/global/common/entity/BaseTimeEntity.java
+++ src/main/java/com/daruda/darudaserver/global/common/entity/BaseTimeEntity.java
@@ -4,16 +4,13 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 
 @Getter
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
 
 	@Column(name = "created_at", updatable = false, nullable = false)
